76a041269bef6b42148dd4518f6f2143681491ed
marel20 <marelli_11@hotmail.com>
2022-09-13 23:02:03 -0300

    Modificacion js

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ const nav = document.querySelector('nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-if(window.scrollY > nav.offsetHeight + 350) {
+if(window.scrollY > nav.offsetHeight + 250) {
     nav.classList.add('active')
 } else {
     nav.classList.remove('active')
