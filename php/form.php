5ca4295b8165e8883d7ee8c328b39d660b4bcd8c
marel20 <marelli_11@hotmail.com>
2022-10-07 17:37:07 -0300

    Modificaciones de paginas

---
+++ php/form.php
@@ -0,0 +1,17 @@
+<?php 
+	$nombre = $_POST['name'];
+	$email = $_POST['email'];
+	$Company = $_POST['Company'];
+	$affair = $_POST['affair'];
+	$mensaje = "Nombre: ".$nombre."\nCompania: ".$Company."\nEmail: ".$email."\nMensaje:\n\n".$_POST['message'];
+
+
+	if(mail('info@mgsolutions.com.ar', $affair, $mensaje)){
+		echo  '<script language="javascript"> alert ("Su consulta fue enviada. Responderemos en breve."); window.location.href="../#contact.html" </script>';
+	} else {
+        echo '<script language="javascript"> alert ("Ocurrio un error inesperado, intente de nuevo mas tarde"); window.location.href="../#contact.html" </script>';
+        }
+ ?>
+
+
+
